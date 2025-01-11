f0f9a1706e388821dd0af0768e9e64b1754e45b3
digiClem <vp@cfaraon.fr>
2025-01-11 02:08:58 +0100

    modify for debug

--- pages/api/spin.js
+++ pages/api/spin.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
     if (req.method !== 'GET') {
       return res.status(405).json({ error: 'Method not allowed' });
     }
-    return res.status(200).json("2");
+    return res.status(200).json("2"); 
     try {
       const { APP_URL, AUTH_TOKEN } = process.env;
   
